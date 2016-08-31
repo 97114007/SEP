d10a9bbe405f2b0b30eb641ba017c6b29396569d
michuts <mich@hotmail.com.au>
2016-08-31 16:19:49 +1000

    Create lostitem.php

---
+++ lostitem.php
@@ -0,0 +1,82 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+</style>
+</head>
+<body>
+<h1>New Lost Item Form</h1>
+<div>
+	Category:*
+	<select>
+		<option value="" disabled selected>Select</option>
+		<option value="categorydatavalue1">CategoryName1</option>
+		<option value="categorydatavalue2">CategoryName2</option>
+		<option value="categorydatavalue3">CategoryName3</option>
+		<option value="categorydatavalue4">CategoryName4</option>
+		<option value="categorydatavalue5">CategoryName5</option>
+	</select>
+</div>
+<div>
+	Sub-Category:*
+	<select>
+		<option value="" disabled selected>Select</option>
+		<option value="subcategorydatavalue1">Sub-CategoryName1</option>
+		<option value="subcategorydatavalue2">Sub-CategoryName2</option>
+		<option value="subcategorydatavalue3">Sub-CategoryName3</option>
+		<option value="subcategorydatavalue4">Sub-CategoryName4</option>
+		<option value="subcategorydatavalue5">Sub-CategoryName5</option>
+	</select>
+</div>
+<div>
+	Colour:*
+	<select>
+		<option value="" disabled selected>Select</option>
+		<option value="colour1">Colour1</option>
+		<option value="colour2">Colour2</option>
+		<option value="colour3">Colour3</option>
+		<option value="colour4">Colour4</option>
+		<option value="colour5">Colour5</option>
+	</select>
+</div>
+<div>
+Date Lost:*
+<input type="number" placeholder="DD" maxlength="2" min="1" max="31">
+<input type="number" placeholder="MM" maxlength="2" min="1" max="12">
+<input type="number" placeholder="YYYY" maxlength="4" min="1900" max="2016">
+</div>
+
+<div>
+	Location Lost:
+	<select>
+		<option value="" disabled selected>Select</option>
+		<option value="location1">Location1</option>
+		<option value="location2">Location2</option>
+		<option value="location3">Location3</option>
+		<option value="location4">Location4</option>
+		<option value="location5">Location5</option>
+	</select>
+</div>
+
+<div>
+Picture:
+<input type="file" id="ImgFile">
+<!--<button onclick="uploadFn()"></button>-->
+<script>
+function uploadFn() {
+	var photo = document.getElementById("ImgFile").accept="image/*";
+}
+</script>
+</div>
+<div>
+Additional Details:
+<br></br>
+<textarea rows="6" cols="50" placeholder="Please enter any other information to identify your lost item"></textarea>
+</div>
+<div><input type="submit" value="Submit"></div>
+</body>
+</html>
