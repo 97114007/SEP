b66590db2379e0a9fb383114f1fc89c4b10b0e99
michuts <mich@hotmail.com.au>
2016-09-05 02:36:56 +1000

    Update main.php

--- main.php
+++ main.php
@@ -74,7 +74,7 @@
               </div>
             </div>
           </section>
-          <a id="addButton" href="lodge-form.php" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+          <a id="addButton" href="lostitemform.php" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
             <i class="material-icons">add</i>
           </a>
         </div>
